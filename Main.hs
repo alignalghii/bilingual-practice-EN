7e4b17d64b56d27cb58dd6e9d73e16e226b8fdca
Endrey Márk <ungazik@gmail.com>
2022-05-17 21:18:21 +0200

    Make difference in typings explicit, a kind of architecture + add  static

--- Main.hs
+++ Main.hs
@@ -2,16 +2,37 @@
 
 module Main (main) where
 
-import Web.Scotty as S hiding (body)
--- import Data.Text.Lazy (Text, pack)
+import Web.Scotty as S
+import Prelude hiding (head)
+import Data.Text.Lazy (Text, pack)
+import Text.Blaze.Html5 as H hiding (map, param, main)
+import Text.Blaze.Html5.Attributes as HA hiding (title, span, form)
+import Text.Blaze.Html.Renderer.Pretty
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.Static (staticPolicy, (>->), noDots, addBase)
+
+blaze :: Html -> ActionM ()
+blaze = S.html . pack . renderHtml
 
 main :: IO ()
 main = scotty 3000 router
 
 router :: ScottyM ()
 router = do
-    get "/" home
+    middleware $ staticPolicy (noDots >-> addBase "static")
+    middleware logStdoutDev
+    get "/" homeAction
 
 
-home :: ActionM ()
-home = html "Hello"
+homeAction :: ActionM ()
+homeAction = blaze homeView
+
+homeView :: Html
+homeView = docTypeHtml $ do
+    head $ do
+        meta ! charset "UTF-8"
+        link ! rel "icon" ! href "img/favicon.ico"
+        title "Magyar-angol szó- és mondatgyakorló"
+    H.body $ do
+        h1 "Magyar-angol szó- és mondatgyakorló"
+        img ! src "img/favicon.png"
