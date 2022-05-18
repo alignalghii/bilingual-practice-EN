c1e2ae28473405207a6b26ce5da53470c832f082
Endrey Márk <ungazik@gmail.com>
2022-05-18 12:12:24 +0200

    Further modularization: from Router, split off BuiltinServer (containing Middleware options)

---
+++ BuiltinServer.hs
@@ -0,0 +1,9 @@
+module BuiltinServer (builtinServerOptions) where
+
+import Network.Wai (Middleware)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.Static (staticPolicy, (>->), noDots, addBase)
+
+
+builtinServerOptions :: [Middleware]
+builtinServerOptions = [staticPolicy (noDots >-> addBase "static"), logStdoutDev]
--- Router.hs
+++ Router.hs
@@ -3,12 +3,10 @@
 module Router where
 
 import Controller.HomeController (homeAction)
+import BuiltinServer (builtinServerOptions)
 import Web.Scotty (ScottyM, middleware, get)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Network.Wai.Middleware.Static (staticPolicy, (>->), noDots, addBase)
 
 router :: ScottyM ()
 router = do
-    middleware $ staticPolicy (noDots >-> addBase "static")
-    middleware logStdoutDev
+    mapM_ middleware builtinServerOptions
     get "/" homeAction
